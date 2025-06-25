7ef556763940667b2db1a2a681bab181d80d6f61
Rohit Dhillon <rohitdhillon983@gmail.com>
2025-06-25 23:18:38 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,17 @@
+
+import express from 'express';
+import mongoose from 'mongoose';
+import cors from 'cors';
+import dotenv from 'dotenv';
+
+const app = express();
+dotenv.config();
+
+app.use(cors());
+app.use(express.json());
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+});
